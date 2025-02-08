ff659410bfcce0bba75f4fc25d781262bb38a6ba
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2025-02-08 12:08:36 +0900

    initial commit

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -13,7 +13,7 @@ struct ScrumdingerApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MeetingView_Previews()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
