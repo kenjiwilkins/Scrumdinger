0e4ed5cf0db27a4619805a2c6667195ba47e29c4
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2025-02-08 13:44:43 +0900

    Add a11y

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -11,12 +11,14 @@ struct CardView: View {
     let scrum: DailyScrum
     var body: some View {
         VStack(alignment:.leading) {
-            Text(scrum.title).font(.title)
+            Text(scrum.title).font(.headline).accessibilityAddTraits(/*@START_MENU_TOKEN@*/.isHeader/*@END_MENU_TOKEN@*/)
             Spacer()
             HStack{
                 Label("\(scrum.attendees.count)", systemImage: "person.3")
+                    .accessibilityLabel("\(scrum.attendees.count) attendees")
                 Spacer()
-                Label("\(scrum.lengthInMinutes)", systemImage: "clock").padding(.trailing, 8)
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock").labelStyle(.trailingIcon)
+                    .accessibilityLabel("\(scrum.lengthInMinutes) minute meeting")
             }.font(.caption)
         }
         .padding()
