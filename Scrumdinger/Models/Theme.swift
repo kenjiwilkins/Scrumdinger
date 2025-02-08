cf4733865e3109422340175bf0acb26e9e6d2ed1
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2025-02-08 13:44:20 +0900

    Fix color

--- Scrumdinger/Models/Theme.swift
+++ Scrumdinger/Models/Theme.swift
@@ -33,6 +33,23 @@ enum Theme: String {
         }
     }
     var mainColor: Color {
-        Color(rawValue)
-    }
+            switch self {
+            case .bubblegum: return Color(red: 1.0, green: 0.8, blue: 0.9)
+            case .buttercup: return Color(red: 1.0, green: 0.9, blue: 0.4)
+            case .indigo: return Color(red: 0.4, green: 0.0, blue: 0.9)
+            case .lavender: return Color(red: 0.9, green: 0.8, blue: 1.0)
+            case .magenta: return Color(red: 1.0, green: 0.0, blue: 1.0)
+            case .navy: return Color(red: 0.0, green: 0.0, blue: 0.5)
+            case .orange: return Color(red: 1.0, green: 0.6, blue: 0.0)
+            case .oxblood: return Color(red: 0.4, green: 0.0, blue: 0.0)
+            case .periwinkle: return Color(red: 0.8, green: 0.8, blue: 1.0)
+            case .poppy: return Color(red: 1.0, green: 0.1, blue: 0.1)
+            case .purple: return Color(red: 0.5, green: 0.0, blue: 0.5)
+            case .seafoam: return Color(red: 0.8, green: 1.0, blue: 0.9)
+            case .sky: return Color(red: 0.4, green: 0.7, blue: 1.0)
+            case .tan: return Color(red: 0.9, green: 0.8, blue: 0.6)
+            case .teal: return Color(red: 0.0, green: 0.5, blue: 0.5)
+            case .yellow: return Color(red: 1.0, green: 1.0, blue: 0.0)
+            }
+        }
 }
